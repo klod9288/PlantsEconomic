72be3db315979e8205d510c352601b558a91f1ec
klod <klod9288@gmail.com>
2017-11-05 11:59:27 +0700

    Create Main Fragment Success

--- app/src/main/java/srongklod_bangtamruat/plantseconomic/MainActivity.java
+++ app/src/main/java/srongklod_bangtamruat/plantseconomic/MainActivity.java
@@ -3,6 +3,8 @@ package srongklod_bangtamruat.plantseconomic;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import srongklod_bangtamruat.plantseconomic.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,7 +12,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        Add Fragment to Activity
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment()).commit();
+        }
 
+    }//onCreate
 
-    }
 }//Main Class
